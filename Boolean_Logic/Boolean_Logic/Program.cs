580bc0b1f5f3723b4b6cf0e4ae41c3656deb8018
clcar23 <92751822+clcar23@users.noreply.github.com>
2022-02-23 19:25:07 -0700

    created Boolean Logic Submission Assignment

---
+++ Boolean_Logic/Boolean_Logic/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Boolean_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // The 'AND' operator
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            // The 'OR' operator
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            // The 'EQUALS' operator
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            // The 'NOT' operator
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            // This is the 'XOR' operator
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Boolean_Logic_Submission_Assignment/Boolean_Logic_Submission_Assignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Boolean_Logic_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This first line gets the users age, converts to an integer
+            Console.WriteLine("What is your age? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            // This line sees if they have a DUI 
+            Console.WriteLine("Have you ever had a DUI? Yes or No");
+            string dui = Console.ReadLine();
+
+            // This line finds how many speeding tickets, converts to an integer
+            Console.WriteLine("How many speeding tickets do you have? ");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            // Here I wrote an if/else statement to get a qualified result or not
+            // I added in different operators to account for all the conditions
+            if (age > 15 && dui == "No" || dui == "no" && tickets <= 3)
+            {
+                Console.WriteLine("What is your age? ");
+                Console.WriteLine(age);
+                Console.WriteLine("Have you ever had a DUI? Yes or No");
+                Console.WriteLine(dui);
+                Console.WriteLine("How many speeding tickets do you have? ");
+                Console.WriteLine(tickets);
+                Console.WriteLine("Qualified? ");
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("What is your age? ");
+                Console.WriteLine(age);
+                Console.WriteLine("Have you ever had a DUI? Yes or No");
+                Console.WriteLine(dui);
+                Console.WriteLine("How many speeding tickets do you have? ");
+                Console.WriteLine(tickets);
+                Console.WriteLine("Qualified? ");
+                Console.WriteLine("False");
+            }
+
+            Console.ReadLine();
+
+
+            
+
+        }
+    }
+}
