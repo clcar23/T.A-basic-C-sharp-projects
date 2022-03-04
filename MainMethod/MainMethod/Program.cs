db39ffab464d5784528b892a63a0e4cfae41ccb3
clcar23 <92751822+clcar23@users.noreply.github.com>
2022-03-04 16:02:33 -0700

    created MainMethod assignment composed of different methods in my own created class, that all perform different functions

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the class 'myMethods' and method 'numMethod', then passing in the integer 6
+            int intResult = myMethods.numMethod(6);
+            Console.WriteLine(intResult);
+
+            // Doing the same as above but calling the 'decimalMethod' and passing in 6.6
+            decimal decimalResult = myMethods.decimalMethod(6.6m);
+            Console.WriteLine(decimalResult);
+
+            // Finally passing a string '66' and getting back an integer
+            int stringResult = myMethods.stringMethod("66");
+            Console.WriteLine(stringResult);
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
---
+++ MainMethod/MainMethod/myMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    // Created the class MainMethod above, below we have created the myMethod method
+    public class myMethods
+    {
+        // This method will take an integer, create a math op for it, then return the answer
+        public static int numMethod(int num)
+        {
+            int numResult = num + 44;
+            return numResult;
+        }
+        // This method will take a decimal, round it down, then return it as an integer to the main program
+        public static decimal decimalMethod(decimal num2)
+        {
+            // Rounding the decimal down
+            decimal decimalResult = Math.Floor(num2);
+            // Returning the converted integer
+            return Convert.ToInt32(decimalResult);
+
+        }
+        // This last method takes in a string number, coverts it to an integer, performs a math op on it, then returns it to the main program
+        public static int stringMethod(string name)
+        {
+            // The conversion to integer
+            int newName = Convert.ToInt32(name);            
+            return newName * 2;
+        }
+    }
+}
