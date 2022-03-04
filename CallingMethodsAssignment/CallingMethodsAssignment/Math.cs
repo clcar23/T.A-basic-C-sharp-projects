9bfd38ece9a4a379b2c7128409d9f8f96b940cb3
clcar23 <92751822+clcar23@users.noreply.github.com>
2022-03-03 17:06:20 -0700

    created assignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Math.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CallingMethodsAssignment
+{
+    // Created class mathMethods
+    public class mathMethods
+    {
+        // Created first method 
+        public static Double methodOne(double num)
+        {           
+            // Inside this method the given numbers exponent of 3 will be found 
+            double resultOne = Math.Pow(num, 3);
+            return resultOne;
+        }
+
+        // Created the second method
+        public static Double methodTwo(double num)
+        {
+            // This operation will find the square root of a given number
+            double resultTwo = Math.Sqrt(num);
+            return resultTwo;
+        }
+
+        //Created the third method
+        public static Double methodThree(double num)
+        {
+            // This operation will round up a decimal number
+            double resultThree = Math.Ceiling(num);
+            return resultThree;
+        }
+
+
+
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Writing to the console and converting the user number to a double, 'just in case'
+            Console.WriteLine("Please enter a number:");
+            double num = Convert.ToDouble(Console.ReadLine());
+
+            // Calling on methodOne from the mathMethods class to operate on their number
+            double resultOne = mathMethods.methodOne(num);
+            Console.WriteLine("Your number to the 3rd power is: " + resultOne );
+
+            // Calling on methodTwo to get the square root
+            double resultTwo = mathMethods.methodTwo(num);
+            Console.WriteLine("The square root of your number is: " + resultTwo );
+
+            // Rounding their number with methodThree called
+            double resultThree = mathMethods.methodThree(num);
+            Console.WriteLine("Your number rounded up is: " + resultThree);
+            Console.ReadLine();
+        }
+        
+    }
+}
