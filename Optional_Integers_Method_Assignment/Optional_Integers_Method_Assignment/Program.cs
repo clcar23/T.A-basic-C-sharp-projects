74b927f1999c29614a1894dd3e6a4aa9a00a7a30
clcar23 <92751822+clcar23@users.noreply.github.com>
2022-03-04 17:57:35 -0700

    created Method submission assignment, has error

---
+++ Optional_Integers_Method_Assignment/Optional_Integers_Method_Assignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Integers_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            mathMethods.calculate();
+
+            try
+            {
+                {
+                    Console.WriteLine("Please enter a whole number: ");
+                    int requiredInt = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Please enter in another whole number, or don't, it doesn't matter: ");
+                    int optionalInt = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine();
+
+                    Console.ReadLine();
+                }
+            }
+            catch (InvalidCastException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ Optional_Integers_Method_Assignment/Optional_Integers_Method_Assignment/mathMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Integers_Method_Assignment
+{
+    public class mathMethods
+    {
+        
+        public int calculate(int requiredInt, int optionalInt = 10)
+        {
+            
+            int result = requiredInt * optionalInt;
+            return result;
+
+        }
+
+        internal static void calculate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
