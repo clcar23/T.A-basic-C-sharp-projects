21ea1f245f052c0565d35730186a6b2fe3d52822
clcar23 <92751822+clcar23@users.noreply.github.com>
2022-03-05 10:25:32 -0700

    added in a void method

--- MainMethod/MainMethod/Program.cs
+++ MainMethod/MainMethod/Program.cs
@@ -10,18 +10,32 @@ namespace MainMethod
     {
         static void Main(string[] args)
         {
-            // Instantiating the class 'myMethods' and method 'numMethod', then passing in the integer 6
-            int intResult = myMethods.numMethod(6);
-            Console.WriteLine(intResult);
+            //// Instantiating the class 'myMethods' and method 'numMethod', then passing in the integer 6
+            //int intResult = myMethods.numMethod(6);
+            //Console.WriteLine(intResult);
 
-            // Doing the same as above but calling the 'decimalMethod' and passing in 6.6
-            decimal decimalResult = myMethods.decimalMethod(6.6m);
-            Console.WriteLine(decimalResult);
+            //// Doing the same as above but calling the 'decimalMethod' and passing in 6.6
+            //decimal decimalResult = myMethods.decimalMethod(6.6m);
+            //Console.WriteLine(decimalResult);
 
-            // Finally passing a string '66' and getting back an integer
-            int stringResult = myMethods.stringMethod("66");
-            Console.WriteLine(stringResult);
+            //// Finally passing a string '66' and getting back an integer
+            //int stringResult = myMethods.stringMethod("66");
+            //Console.WriteLine(stringResult);
 
+
+
+
+            // Here is a demonstration of a Void Method
+            // First instantiating the method
+            myMethods myMethods = new myMethods();
+
+            // Assigning values to the parameters
+            int num1 = 1;
+            int num2 = 2;
+            // Calling the method and passing in our values
+            myMethods.voidMethod(num1, num2);
+
+            
             Console.ReadLine();
         }
 
--- MainMethod/MainMethod/myMethods.cs
+++ MainMethod/MainMethod/myMethods.cs
@@ -9,27 +9,43 @@ namespace MainMethod
     // Created the class MainMethod above, below we have created the myMethod method
     public class myMethods
     {
-        // This method will take an integer, create a math op for it, then return the answer
-        public static int numMethod(int num)
-        {
-            int numResult = num + 44;
-            return numResult;
-        }
-        // This method will take a decimal, round it down, then return it as an integer to the main program
-        public static decimal decimalMethod(decimal num2)
-        {
-            // Rounding the decimal down
-            decimal decimalResult = Math.Floor(num2);
-            // Returning the converted integer
-            return Convert.ToInt32(decimalResult);
+        //// This method will take an integer, create a math op for it, then return the answer
+        //public static int numMethod(int num)
+        //{
+        //    int numResult = num + 44;
+        //    return numResult;
+        //}
+        //// This method will take a decimal, round it down, then return it as an integer to the main program
+        //public static decimal decimalMethod(decimal num2)
+        //{
+        //    // Rounding the decimal down
+        //    decimal decimalResult = Math.Floor(num2);
+        //    // Returning the converted integer
+        //    return Convert.ToInt32(decimalResult);
 
-        }
-        // This last method takes in a string number, coverts it to an integer, performs a math op on it, then returns it to the main program
-        public static int stringMethod(string name)
+        //}
+        //// This last method takes in a string number, coverts it to an integer, performs a math op on it, then returns it to the main program
+        //public static int stringMethod(string name)
+        //{
+        //    // The conversion to integer
+        //    int newName = Convert.ToInt32(name);            
+        //    return newName * 2;
+        //}
+
+
+
+
+
+        // ************ This is the beginning of a Void Method *************
+        // Create the voiMethod that takes in 2 arguements
+        public static void voidMethod(int num1, int num2)
         {
-            // The conversion to integer
-            int newName = Convert.ToInt32(name);            
-            return newName * 2;
+            // Creating the variable 'result' that performs a math operation in the first parameter
+            int result = num1 + 10;
+            // Displaying the second value to the console
+            Console.WriteLine(num2);
+            
+            
         }
     }
 }
