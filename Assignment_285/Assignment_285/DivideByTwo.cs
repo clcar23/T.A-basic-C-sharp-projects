d240c2d55217a8e1c6d42eeb5f90722e3643eb1e
clcar23 <92751822+clcar23@users.noreply.github.com>
2022-03-06 22:53:09 -0700

    unfinished, errors

---
+++ Assignment_285/Assignment_285/DivideByTwo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_285
+{
+
+    class DivideByTwo
+    {
+        // created a method to take input, perform math on it, send it back to main program
+        static void PrintQuotient(int num)
+        {
+            int result = num / 2;
+            Console.WriteLine("Your number {0}, divided by two is {1}.", num, result);
+        }
+    }
+}
---
+++ Assignment_285/Assignment_285/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_285
+{
+    class Program
+    {
+        // Instantiate the class
+        DivideByTwo divideByTwo = new DivideByTwo();
+        
+        static void Main(string[] args)
+        {
+            // create the method to take the users input
+            void UserNumber(out int num)
+            {
+                Console.WriteLine("Please enter a number: ");
+                num = Convert.ToInt32(Console.ReadLine());
+            }            
+            Console.ReadLine();
+
+            
+           
+                  
+        }
+        int x, y;
+        public static void Multiplication(out x, out y)
+        {
+            
+            Console.WriteLine("{0} multiplied by {1} = {2}", x, y, z);
+            z = 5;
+            z *= z;
+        }
+        
+    }
+}
