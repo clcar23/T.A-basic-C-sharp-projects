ac516f80245b9fa4411907cfbcd90616d3be9166
clcar23 <92751822+clcar23@users.noreply.github.com>
2022-03-08 23:09:42 -0700

    created abstract class assignment, has errors

---
+++ Abstract_class_assignment/Abstract_class_assignment/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_class_assignment
+{
+    abstract public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public abstract void SayName();
+            
+    };
+}
