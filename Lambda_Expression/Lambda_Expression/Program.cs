dabc2016e7fdf822b98c4d029efb2aa83eae9a5e
clcar23 <92751822+clcar23@users.noreply.github.com>
2022-03-14 17:50:05 -0600

    fixing code

--- Lambda_Expression/Lambda_Expression/Program.cs
+++ Lambda_Expression/Lambda_Expression/Program.cs
@@ -8,11 +8,11 @@ namespace Lambda_Expression
 {
     public class Program
     {
-        public void Main(string[] args)
+        static void Main(string[] args)
         {
 
             // creating a list of employees
-            List<Employee> employees = new List<Employee>
+            List<Employee> employee = new List<Employee>
             {
             // adding names and id's to the list
             new Employee { FirstName = "Barry", LastName = "Aardvark", Id = 2},
@@ -28,23 +28,27 @@ namespace Lambda_Expression
             };
 
             // creating a foreach loop to run through the list and find employees named 'Joe' and add them to a new list
-            foreach (List<string> z in employees)
+            List<Employee> joeList = new List<Employee>();
+            foreach (var e in employee)
             {
-                if (Employee.FirstName == "Joe")
+                if (e.FirstName == "Joe")
                 {
-                    Employee.newList.Add(z);
+                    joeList.Add(e);
+                    Console.WriteLine(e + "was added to the list.");
                 }
             }
 
             // created a lambda expression to do the same only shortened, then print to the console
-            List<Employee> newList1 = employees.Where(x => x.FirstName == "Joe").ToList();
-            Console.WriteLine(newList1);
+            List<Employee> newList1 = employee.Where(x => x.FirstName == "Joe").ToList();
+            newList1.ForEach(x => Console.WriteLine(x));
+            
 
             // created an expression to go thru the list and create a new list with Id's greater than 5
-            List<Employee> newList2 = employees.Where(y => y.Id > 5).ToList();
-            Console.WriteLine(newList2);
+            List<Employee> newList2 = employee.Where(y => y.Id > 5).ToList();
+            
             Console.ReadLine();
 
+            
         }
 
         // created class Employee
