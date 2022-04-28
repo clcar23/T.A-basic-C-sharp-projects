05b543f004b82f90657d458b7145ea2d36328549
clcar23 <92751822+clcar23@users.noreply.github.com>
2022-04-28 17:53:20 -0600

    Practice with Lambda expressions

---
+++ Lambda_Expression2/Lambda_Expression2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Lambda_Expression2
+{
+    class Program
+    {
+        // Here are a couple examples of using Lambdas
+        static void Main(string[] args)
+        {
+            // First we create a list of numbers
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+
+            // Next we put in our Lambda expression
+            Func<int, int> squared = x => x * x;
+
+            // Then we call the function, enter a number, and print to the console
+            Console.WriteLine(squared(5));
+
+            // Here is another
+            Func<int, bool> checkOdd = x => x % 2 != 0;
+
+            var oddNumbers = numbers.Where(checkOdd).ToList();
+
+            oddNumbers.ForEach(x => Console.WriteLine(x));
+
+            // This is an example of a Lambda statement
+            Action<int> exponent = x =>
+            {
+                var square = x * x;
+                var cube = x * x * x;
+                Console.WriteLine($"{x} : Squared = {square}, and cubed = {cube}");
+            };
+
+            var otherNumbers = numbers.Where(checkOdd).ToList();
+
+            otherNumbers.ForEach(exponent);
+            
+            Console.ReadLine();
+        }
+    }
+}
